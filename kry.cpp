e4ce90810d23e50c1051849f9478a65d4f6eeb8b
b-vit <46031140+bartak-v@users.noreply.github.com>
2022-04-14 21:15:48 +0200

    uglify

--- kry.cpp
+++ kry.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
     Projekt: RSA and integer factorization
     Autor:   Vít Barták
     Datum:   10.03.2022
@@ -12,17 +12,26 @@ using namespace std;
 
 gmp_randstate_t state;
 
-
 // Generate public and private RSA key with the given length
 // Output P Q N E D
-void rsa_generate(string num_of_bits){
-    num_of_bits="NOT IMPLEMENTED";
+void rsa_generate(string num_of_bits)
+{
+    num_of_bits = "NOT IMPLEMENTED";
     cout << num_of_bits << "\n";
     return;
 }
 
+// Break the RSA by prime factorization
+// Output D
+void rsa_break(string public_modulus)
+{
+    public_modulus = "NOT IMPLEMENTED";
+    cout << public_modulus << "\n";
+    return;
+}
+
 // Encrypts or decrypts the message by the RSA equation C = M ^ E mod N | M = C ^ D mod N
-char* rsa_enc_dec(mpz_t exponent, mpz_t modulus, mpz_t message)
+char *rsa_enc_dec(mpz_t exponent, mpz_t modulus, mpz_t message)
 {
     mpz_t p_c; // Plaintext or cryptogram, based on which operation we need
     mpz_init(p_c);
@@ -31,73 +40,115 @@ char* rsa_enc_dec(mpz_t exponent, mpz_t modulus, mpz_t message)
 }
 
 // Encrypt message M with given E and N
-void rsa_encrypt(string const public_exponent, string public_modulus, string message){
+// Output C
+void rsa_encrypt(string const public_exponent, string public_modulus, string message)
+{
     // Set the inner representation of E,N,M in GNU MP
-    mpz_t exponent,modulus,plaintext;
-    mpz_init_set_str(exponent, public_exponent.c_str(), 0); 
+    mpz_t exponent, modulus, plaintext;
+    mpz_init_set_str(exponent, public_exponent.c_str(), 0);
     mpz_init_set_str(modulus, public_modulus.c_str(), 0);
-    mpz_init_set_str(plaintext, message.c_str(), 0); 
-    cout <<"0x" << rsa_enc_dec(exponent,modulus,plaintext) << "\n";
+    mpz_init_set_str(plaintext, message.c_str(), 0);
+    cout << "0x" << rsa_enc_dec(exponent, modulus, plaintext) << "\n";
     return;
 }
 
-void rsa_decrypt(string private_exponent, string public_modulus, string ciphertext){
+// Decrypt cryptogram C with given D and N
+// Output M
+void rsa_decrypt(string private_exponent, string public_modulus, string ciphertext)
+{
     // Set the inner representation of D,N,C in GNU MP
-    mpz_t exponent,modulus,cryptogram;
-    mpz_init_set_str(exponent, private_exponent.c_str(), 0); 
+    mpz_t exponent, modulus, cryptogram;
+    mpz_init_set_str(exponent, private_exponent.c_str(), 0);
     mpz_init_set_str(modulus, public_modulus.c_str(), 0);
-    mpz_init_set_str(cryptogram, ciphertext.c_str(), 0); 
-    cout <<"0x" << rsa_enc_dec(exponent,modulus,cryptogram) << "\n";
-    return;
-}
-void rsa_break(string public_modulus){
-    public_modulus="NOT IMPLEMENTED";
-    cout << public_modulus << "\n";
+    mpz_init_set_str(cryptogram, ciphertext.c_str(), 0);
+    cout << "0x" << rsa_enc_dec(exponent, modulus, cryptogram) << "\n";
     return;
 }
 
 // Modes of RSA operation
-enum RSA_Operation { gen, encr, decr, br};
+enum RSA_Operation
+{
+    gen,
+    encr,
+    decr,
+    br
+};
 RSA_Operation current_op;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     gmp_randinit_default(state); // Initialize the GNU MP rand state
-    // Get the option
-    string opt="";
-    if(argc >= 2) opt = argv[1]; 
+    string opt = "";             // Get the option
+    if (argc >= 2)
+        opt = argv[1];
     // Set the option to be compared
-    if(opt.compare("-g")  == 0) current_op = gen;
-    else if(opt.compare("-e")  == 0) current_op = encr;
-    else if(opt.compare("-d")  == 0) current_op = decr;
-    else if(opt.compare("-b")  == 0) current_op = br;
-    else{ cout << "You need to set the correct mode of operation (-g -e -d -b)" << endl; return EXIT_FAILURE;}
-    
+    if (opt.compare("-g") == 0)
+        current_op = gen;
+    else if (opt.compare("-e") == 0)
+        current_op = encr;
+    else if (opt.compare("-d") == 0)
+        current_op = decr;
+    else if (opt.compare("-b") == 0)
+        current_op = br;
+    else
+    {
+        cout << "You need to set the correct mode of operation (-g -e -d -b)" << endl;
+        return EXIT_FAILURE;
+    }
 
     // Check arguments and call the proper functions (expecting user to know what inputs to give us).
-    switch(current_op) {
-    
-        case RSA_Operation::gen:
-            if(argc < 3){cout<<"You need to set the number of bits (B) to be generated (./kry -g B)" <<endl; break;}
-            else rsa_generate(argv[2]);
+    switch (current_op)
+    {
+
+    case RSA_Operation::gen:
+        if (argc < 3)
+        {
+            cout << "You need to set the number of bits (B) to be generated (./kry -g B)" << endl;
             break;
-    
-        case RSA_Operation::encr:
-            if(argc < 5){cout<<"You need to set the public exponent (E), public modulus (N), and message (M) to be encrypted (./kry -e E N M)" << endl ; break;}
-            else {rsa_encrypt(argv[2],argv[3],argv[4]);}
+        }
+        else
+            rsa_generate(argv[2]);
+        break;
+
+    case RSA_Operation::encr:
+        if (argc < 5)
+        {
+            cout << "You need to set the public exponent (E), public modulus (N), and message (M) to be encrypted (./kry -e E N M)" << endl;
             break;
-    
-        case RSA_Operation::decr:
-            if(argc < 5){cout<<"You need to set the private exponent (D), public modulus (N), and ciphertext (C) to be decrypted (./kry -d D N C)" << endl ; break;}
-            else {rsa_decrypt(argv[2],argv[3],argv[4]);}
+        }
+        else
+        {
+            rsa_encrypt(argv[2], argv[3], argv[4]);
+        }
+        break;
+
+    case RSA_Operation::decr:
+        if (argc < 5)
+        {
+            cout << "You need to set the private exponent (D), public modulus (N), and ciphertext (C) to be decrypted (./kry -d D N C)" << endl;
             break;
-    
-        case RSA_Operation::br:
-            if(argc < 3){cout<<"You need to set public modulus (N) to be factorized" <<"(./kry -b N)" << endl ; break;}
-            else {rsa_break(argv[2]);}
+        }
+        else
+        {
+            rsa_decrypt(argv[2], argv[3], argv[4]);
+        }
+        break;
+
+    case RSA_Operation::br:
+        if (argc < 3)
+        {
+            cout << "You need to set public modulus (N) to be factorized"
+                 << "(./kry -b N)" << endl;
             break;
-    
-        default:
-            break;
+        }
+        else
+        {
+            rsa_break(argv[2]);
+        }
+        break;
+
+    default:
+        break;
     }
     return EXIT_SUCCESS;
 }
